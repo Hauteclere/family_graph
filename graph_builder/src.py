e824c576ca9c18cf72f2846d69f1c6229edd50b7
Hauteclere <oliver.f.lavers@gmail.com>
2023-12-04 17:00:10 +1000

    refactor

--- graph_builder/src.py
+++ graph_builder/src.py
@@ -1,117 +1,92 @@
 from pathlib import Path, PosixPath
 import markdown
 import lxml.html
-import os
-import re
-
-all_edges = set()
-hyperlinks = dict()
 
 class GraphBuilderError(ValueError):
     def __init__(self, message):
         self.message = message
         super().__init__()
 
-class NoHeadingError(GraphBuilderError):
-    pass
-
 class DuplicateHeadingsError(GraphBuilderError):
     pass
 
-def get_absolute_path(somepath: str, parent: PosixPath):
-    if not len(somepath):
-        return None
-    if somepath[0] == "/":
-        return somepath
-    somepath = somepath.split('#')[0]
-    return (parent / somepath).resolve()
+class GraphedDirectory():
+    def __init__(self, path_string: str) -> None:
+        self.p_path = Path(path_string)
+        self.files = {
+            (self.p_path.parent.resolve() / str(eachpath)).resolve(): GraphedFile(
+                self,
+                eachpath
+            ) for eachpath in self.markdown_file_addresses
+        }
 
-def is_in_parent_folder(somepath: PosixPath, somefolder: PosixPath):
-    return somefolder in Path(somepath).parents and os.path.exists(somepath)
-
-def get_heading_from_doctree(doctree: lxml.html):
-    headings = doctree.xpath('//h1')
-    if not headings:
-        raise NoHeadingError("No heading found!")
-    return headings[0].text
-
-def filter_to_local_links(links, localpath):
-    return [
-        locallink for locallink in (
-            link for link in links
-        ) if is_in_parent_folder(locallink, localpath.resolve())
-    ]
-
-def is_markdown_filepath(filepath: Path):
-    return (
-        isinstance(filepath, Path) and (
-            bool(re.match(r"^.+\.md$", str(filepath))) or
-            bool(re.match(r"^.+\.md#.+$", str(filepath)))
-        )
-    )
+    @property
+    def markdown_file_addresses(self) -> set:
+        return {
+            eachpath for eachpath in self.p_path.rglob(
+                '*.md'
+            ) 
+        }
     
-def absolute_links_from_doctree(doctree: lxml.html, filepath: PosixPath):
-    return filter(
-        lambda x: is_markdown_filepath(x),
-        (
-            get_absolute_path(
-                eachlink.get('href'),
-                filepath.parent.resolve()
-            ) for eachlink in doctree.xpath('//a')
-        )
-    )
+    @property
+    def graph_details(self):
+        nodes = set()
 
-def heading_and_links_from_filepath(filepath: PosixPath, localpath: PosixPath):
-    with open(filepath) as file:
-        markdown_string = file.read()
-        html_string = markdown.markdown(markdown_string)
-        doctree = lxml.html.fromstring(html_string)
-        
-        all_links = absolute_links_from_doctree(
-            doctree,
-            filepath
-        )
+        for path, file in self.files.items():
+            heading = file.heading
+            if heading is None:
+                continue
+            if heading in nodes:
+                bad_files = [
+                    str(badpath) for badpath, bad_file in self.files.items() if bad_file.heading == file.heading
+                ]
+                message = f"Multiple pages have the same heading: {' and '.join(bad_files)}."
+                
+                raise DuplicateHeadingsError(message)
+            nodes.add(heading)
         
         return {
-            "heading": get_heading_from_doctree(doctree),
-            "links": filter_to_local_links(
-                all_links, 
-                localpath
-            )
+            "nodes": nodes,
+            "edges": {
+                (
+                    file.heading, 
+                    self.files[link].heading
+                ) for file in self.files.values() for link in file.links if link in self.files.keys()
+            }
         }
-        
-def all_headings_and_links(somefolder: PosixPath):
-    result = dict()
+
     
-    for eachpath in somefolder.rglob('*.md'):
-        try:
-            result[eachpath.resolve()] = heading_and_links_from_filepath(eachpath, somefolder)
-        except NoHeadingError:
-            print(f"\t...WARNING: file at {eachpath.resolve()} is malformed - no heading. \n\tThis file will not be added to the graph.")
-    return result
+class GraphedFile():
+    def __init__(self, graphed_dir: GraphedDirectory, p_path: PosixPath) -> None:
+        self.p_path = p_path
 
-def get_edges(all_headings_and_links: dict):
-    edges = set()
-    for eachfile, heading_and_links in all_headings_and_links.items():
-        for link in heading_and_links["links"]:
-            edge = tuple(
-                    sorted(
-                        [
-                            heading_and_links["heading"], 
-                            all_headings_and_links[link]["heading"]
-                        ]
-                    )
-                )
-            if len(set(edge)) == 2:
-                edges.add(edge)
-    return edges
-
-def get_nodes(all_headings_and_links: dict):
-    nodes = set()
-    for eachfile, eachdetails in all_headings_and_links.items():
-        if eachdetails["heading"] in nodes:
-            message = f"Multiple pages have the same heading: {' and '.join([str(otherfile) for otherfile, otherdetails in all_headings_and_links.items() if otherdetails['heading']==eachdetails['heading']])}"
-            raise DuplicateHeadingsError(message)
+        with open(self.p_path) as file:
+            self.graphed_dir = graphed_dir
+            self.doctree = lxml.html.fromstring(markdown.markdown(file.read()))
+    
+    @property
+    def heading(self) -> str:
+        headings = self.doctree.xpath('//h1')
+        if not headings:
+            print(f"\t...WARNING: file at {self.p_path.resolve()} is malformed - no heading. \n\t\tThis file will not be added to the graph.")
+            return None
+        return headings[0].text
+    
+    @property
+    def links(self) -> set:
+        return {
+            self.get_absolute_path(
+                eachlink.get('href')
+            ) for eachlink in self.doctree.xpath('//a')
+        }
+    
+    def get_absolute_path(self, somepath: str) -> Path:
+        if not isinstance(somepath, str) or not len(somepath):
+            return Path("/there/is/no/path")
+        if somepath[0] == "/":
+            return somepath
+        return (self.p_path.parent.resolve() / somepath.split('#')[0]).resolve()
         
-        nodes.add(eachdetails["heading"])
-    return nodes
+
+    
+    
--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from pathlib import Path
 if __name__=="__main__":
     print("RUNNING GRAPH BUILDER...\n--------------------------------------------\n")
     try:
-        data = src.all_headings_and_links(Path("./files"))
-        nodes_and_edges = {
-            "nodes": src.get_nodes(data),
-            "edges": src.get_edges(data)
-        }
-
+        folder = src.GraphedDirectory(Path("./files"))
+        details = folder.graph_details
         print("\n--------------------------------------------\nNODES: ")
-        print(f"\t{nodes_and_edges['nodes']}")
+        print(f"\t{details['nodes']}")
         print("EDGES")
-        for edge in nodes_and_edges['edges']:
+        for edge in details['edges']:
             print(f"\t{edge}")
     except src.GraphBuilderError as e:
         print(f"\n--------------------------------------------\nERROR - TERMINATING: {e.message}")
